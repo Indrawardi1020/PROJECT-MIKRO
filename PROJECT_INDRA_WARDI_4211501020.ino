d20f2c94abfbfe535d4d85f7e65a7ab9eb92fe80
Indrawardi1020 <Indrawardi32@gmail.com>
2016-12-08 23:35:29 -0800

    PROJECT OHMMETER

---
+++ PROJECT_INDRA_WARDI_4211501020.ino
@@ -0,0 +1,30 @@
+float V;
+float Vin  = 5;
+float Vout = 0;
+float R1= 10;
+int R2= 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  V= analogRead(A0);
+  Serial.print("V : ");
+  Serial.println(V);
+  Vout= (V*Vin)/1023;
+  Serial.print("Vout : ");
+  Serial.println(Vout);
+  
+  R2= (Vout*R1)/(Vin-Vout);
+  
+  Serial.print("R2: ");
+  
+  Serial.println(R2);
+
+  delay(1000);
+  
+}
+
